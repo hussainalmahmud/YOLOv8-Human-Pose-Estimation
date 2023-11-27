75d9c13a736572111ae3f3b839e2f31830bacb14
hussainsan <hussain.almahmud1@outlook.com>
2023-11-27 17:01:43 +0900

    add code

--- yolov8l-pose9.py
+++ yolov8l-pose9.py
@@ -1,4 +1,3 @@
-import os
 import cv2
 import argparse
 import numpy as np
@@ -9,7 +8,7 @@ from ultralytics.utils.files import increment_path
 def pose_estimation(model, source, view_img=False, save_img=False, exist_ok=False):
     
     pose_model = YOLO(model)
-    det_model = YOLO('yolov8l.pt')
+    det_model = YOLO('models/yolov8l.pt')
     
     
     if not Path(source).exists(): # Check sourcwqe path
@@ -36,64 +35,34 @@ def pose_estimation(model, source, view_img=False, save_img=False, exist_ok=Fals
             # Object detection and tracking
             det_results = det_model.track(frame, save=True, conf=0.25, iou=0.7, persist=True,device='cpu', classes=[0])
             pose_results = pose_model.track(frame, save=True, conf=0.25, iou=0.7, persist=True,device='cpu')
-            for det_box, pose_box in zip(det_results[0].boxes, pose_results[0].boxes):
-                avg_conf = (det_box.conf + pose_box.conf) / 2
-                # print("avg_conf ", avg_conf)
-            # img_annotated = pos_results[0].plot(boxes=True)
-            # img_annotated = frame.copy()
+            # for det_box, pose_box in zip(det_results[0].boxes, pose_results[0].boxes):
+            #     avg_conf = (det_box.conf + pose_box.conf) / 2
+            #     # print("avg_conf ", avg_conf)
             img_annotated_det = det_results[0].plot()
             img_annotated_pose = pose_results[0].plot()
             alpha = 0.5  # Alpha controls the transparency: 0 is fully transparent, 1 is fully opaque
             img_annotated = cv2.addWeighted(img_annotated_det, alpha, img_annotated_pose, 1-alpha, 0)
 
-            # Process tracking lines
-            if avg_conf > 0.5:
-                # img_annotated_det = det_results[0].plot()
-                # img_annotated = img_annotated_det.copy()
                 
-                if det_results[0].boxes.id is not None:
-                    boxes = det_results[0].boxes.xyxy.cpu()
-                    track_ids = det_results[0].boxes.id.int().cpu().tolist()
+            if det_results[0].boxes.id is not None:
+                boxes = det_results[0].boxes.xyxy.cpu()
+                track_ids = det_results[0].boxes.id.int().cpu().tolist()
 
-                    for box, track_id in zip(boxes, track_ids):
-                        bbox_center = (box[0] + box[2]) / 2, (box[1] + box[3]) / 2 
+                for box, track_id in zip(boxes, track_ids):
+                    bbox_center = (box[0] + box[2]) / 2, (box[1] + box[3]) / 2 
 
-                        # Get or create track history for this track_id
-                        track = track_history.get(track_id, [])
-                        track.append((float(bbox_center[0]), float(bbox_center[1])))
+                    # Get or create track history for this track_id
+                    track = track_history.get(track_id, [])
+                    track.append((float(bbox_center[0]), float(bbox_center[1])))
 
-                        if len(track) > 30:
-                            track.pop(0)
+                    if len(track) > 30:
+                        track.pop(0)
 
-                        track_history[track_id] = track
+                    track_history[track_id] = track
 
-                        # Draw tracking line
-                        points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-                        cv2.polylines(img_annotated, [points], isClosed=False, color=(0, 0, 255), thickness=2)
-
-            else:
-                if pose_results[0].boxes.id is not None:
-                    # img_annotated_pose = pose_results[0].plot()
-                    # img_annotated = img_annotated_pose.copy()
-                    
-                    boxes = pose_results[0].boxes.xyxy.cpu()
-                    track_ids = pose_results[0].boxes.id.int().cpu().tolist()
-
-                    for box, track_id in zip(boxes, track_ids):
-                        bbox_center = (box[0] + box[2]) / 2, (box[1] + box[3]) / 2 
-
-                        # Get or create track history for this track_id
-                        track = track_history.get(track_id, [])
-                        track.append((float(bbox_center[0]), float(bbox_center[1])))
-
-                        if len(track) > 30:
-                            track.pop(0)
-
-                        track_history[track_id] = track
-
-                        # Draw tracking line
-                        points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-                        cv2.polylines(img_annotated, [points], isClosed=False, color=(0, 0, 255), thickness=2)
+                    # Draw tracking line
+                    points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                    cv2.polylines(img_annotated, [points], isClosed=False, color=(0, 0, 255), thickness=2)
             
             if view_img:
                 cv2.imshow("Video Pose Estimation", img_annotated)
@@ -118,7 +87,7 @@ def pose_estimation(model, source, view_img=False, save_img=False, exist_ok=Fals
 def parse_opt():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default='yolov8l-pose.pt', help='initial weights path')
+    parser.add_argument('--model', type=str, default='models/yolov8l-pose.pt', help='initial weights path')
     parser.add_argument('--source', type=str,default='People-Walking-2.mp4' , help='video file path')
     parser.add_argument('--view-img', action='store_true', help='show results')
     parser.add_argument('--save-img', action='store_true', help='save results')
@@ -131,11 +100,6 @@ def main(opt):
     pose_estimation(**vars(opt))
 
 if __name__ == '__main__':
-
     opt = parse_opt()
-
     main(opt)
-
-
-
 
