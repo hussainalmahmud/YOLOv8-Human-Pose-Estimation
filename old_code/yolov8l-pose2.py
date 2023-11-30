990d75796b9012c9e2c134e8c89171fbedee0d18
hussainsan <hussain.almahmud1@outlook.com>
2023-12-01 08:21:02 +0900

    remove old code

--- old_code/yolov8l-pose2.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load an official or custom model
-model = YOLO('yolov8x-pose.pt')  # Load an official Pose model
-
-source = 'People-Walking-2.mp4'
-
-# Perform tracking with the model
-results = model.track(source, save=True, conf=0.3, iou=0.7, persist=True, show=True)  # Tracking with default tracker
--- old_code/yolov8l-pose4.py
+++
@@ -1,5 +0,0 @@
-    """
-    Implementing Temporal Smoothing: You can implement a form of temporal smoothing where detections are not treated
-    independently frame by frame, but rather in the context of previous frames. This can be as simple as maintaining a cache 
-    of recent detections and using this information to infer the presence of a person even if they are momentarily not detected.
-    """
